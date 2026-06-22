e70c63370fb60d10ef950458e2ac8d4122381caf
besufikad-a <besufikad@uchicago.edu>
2026-06-22 18:42:53 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s = list(s)
+        t = list(t)
+        
+        for char in s:
+            if char in t:
+                t.remove(char)
+            else:
+                return False
+        return True
+
+            
+            
